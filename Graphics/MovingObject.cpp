c2fc2e786665d3b145cc590744492873bce8725f
Alisa Wallace <akwallace2014@gmail.com>
2021-12-01 15:18:49 -0800

    Created a class for handling the translation of the fairy objects along a path

---
+++ Graphics/MovingObject.cpp
@@ -0,0 +1,61 @@
+#include "MovingObject.h"
+
+MovingObject::MovingObject(std::vector<vec2> targetPositions, int startIndex, float movementFactor) {
+	if (targetPositions.size() < 2) {
+		printf("\nMovingObject: error, not enough target positions");
+	}
+
+	targets = targetPositions;
+	currentIndex = startIndex;
+	movementRate = movementFactor;
+
+	if (currentIndex < targets.size() - 1) {
+		targetIndex = currentIndex + 1;
+	}
+	else {
+		targetIndex = 0;
+	}
+
+	updateTarget();
+}
+
+void MovingObject::adjustMovement() {
+	currentPos += increments;
+	distMoved.x += abs(increments.x);
+	distMoved.y += abs(increments.y);
+
+	if (abs(distMoved.x) > abs(totalDistance.x)) {
+		incrementIndex();
+	}
+}
+
+float MovingObject::x() {
+	return currentPos.x;
+}
+
+float MovingObject::y() {
+	return currentPos.y;
+}
+
+void MovingObject::incrementIndex() {
+	++currentIndex;
+	if (currentIndex > targets.size() - 1) {
+		currentIndex = 0;
+	}
+
+	targetIndex = currentIndex + 1;
+	if (targetIndex > targets.size() - 1) {
+		targetIndex = 0;
+	}
+
+	updateTarget();
+}
+
+void MovingObject::updateTarget() {
+	currentPos = targets.at(currentIndex);
+	movingFrom = currentPos;
+	movingTowards = targets.at(targetIndex);
+	totalDistance = movingTowards - currentPos;
+	increments = totalDistance * movementRate;
+	distMoved = vec2(0.0);
+}
---
+++ include/MovingObject.h
@@ -0,0 +1,56 @@
+#pragma once
+#ifndef MOVINGOBJECT_H
+#define MOVINGOBJECT_H
+
+#include <vector>
+#include "glad.h"
+#include "VecMat.h"
+
+class MovingObject {
+public:
+    // Constructor
+	MovingObject(std::vector<vec2> targetPositions, int startIndex = 0, float movementFactor = 0.002);
+
+    // adjusts the object's location coordinates 
+	void adjustMovement();
+
+    // returns the object's current respective coordinate
+    float x();
+    float y();
+
+private:
+    // factor by which to adjust coordinates in each call to adjustMovement
+    float movementRate;
+
+    // coordinates of target positions for the object to move towards
+    std::vector<vec2> targets;
+
+    // indices in targets vector
+    int currentIndex;
+    int targetIndex;
+
+    // coordinates of origin - will not change until target updated
+    // equal to targets[currentIndex]
+    vec2 movingFrom;
+
+    // target coordinates - will not change until target updated
+    // equal to targets[targetIndex]
+    vec2 movingTowards;
+
+    // total distance needed to move from current location to target location
+    vec2 totalDistance;
+   
+    // current coordinates of the object - will change as movement adjusted
+    vec2 currentPos;
+
+    // adjustments made to current coordinates to move them towards the target position
+    vec2 increments;
+
+    // total distance current moved from origin location to current location
+    vec2 distMoved;
+
+    void incrementIndex();
+
+    void updateTarget();
+};
+#endif // !MOVINGOBJECT_H
